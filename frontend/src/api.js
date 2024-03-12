63333df9d8eb778160a902d8f4fb67e8235d9b7f
Magomed <ingushotdushi@mail.ru>
2024-03-12 19:16:30 +0300

    final commit

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,16 +2,12 @@ import { cryptoAssets, cryptoData } from './data'
 
 export function fakeFetchCrypto() {
 	return new Promise((resolve) => {
-		setTimeout(() => {
-			resolve(cryptoData)
-		}, 500)
+		resolve(cryptoData)
 	})
 }
 
 export function fetchAssets() {
 	return new Promise((resolve) => {
-		setTimeout(() => {
-			resolve(cryptoAssets)
-		}, 100)
+		resolve(cryptoAssets)
 	})
 }
