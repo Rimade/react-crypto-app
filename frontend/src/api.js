eaf7c4c737af350ee141b95faf6b8662eadb2848
Magomed <ingushotdushi@mail.ru>
2024-03-12 19:13:04 +0300

    add api

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -4,7 +4,7 @@ export function fakeFetchCrypto() {
 	return new Promise((resolve) => {
 		setTimeout(() => {
 			resolve(cryptoData)
-		}, 5)
+		}, 500)
 	})
 }
 
@@ -12,6 +12,6 @@ export function fetchAssets() {
 	return new Promise((resolve) => {
 		setTimeout(() => {
 			resolve(cryptoAssets)
-		}, 5)
+		}, 100)
 	})
 }
